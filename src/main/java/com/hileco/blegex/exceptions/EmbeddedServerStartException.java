aa60bbdbd462d7a1c81f61f3af81330b9097ff1e
Philipp Gayret <pgayret@gmail.com>
2014-06-20 21:40:45 +0200

    Added local embedded jetty as in hileco-drpc modules

---
+++ src/main/java/com/hileco/blegex/exceptions/EmbeddedServerStartException.java
@@ -0,0 +1,9 @@
+package com.hileco.blegex.exceptions;
+
+public class EmbeddedServerStartException extends RuntimeException {
+
+    public EmbeddedServerStartException(Throwable cause) {
+        super(cause);
+    }
+
+}
