65e9bcf767641f47c2a5ea8151a15914b71b22d7
Philipp Gayret <pgayret@gmail.com>
2014-06-24 23:51:33 +0200

    Added AngularJS form validation for login and register, using the stub services

--- src/main/webapp/js/modules/blegex.js
+++ src/main/webapp/js/modules/blegex.js
@@ -1,4 +1,12 @@
-angular.module('blegex', ['ngRoute', 'ui.bootstrap']);
+angular.module('blegex', ['ngRoute', 'ui.bootstrap', 'blegex.user', 'blegex.ui']);
+
+/**
+ * Puts both UI and Authentication on the $rootScope for easy referencing from the views.
+ */
+angular.module('blegex.ui').run(function ($rootScope, UI, Authentication) {
+    $rootScope.ui = UI;
+    $rootScope.authentication = Authentication;
+});
 
 angular.module('blegex').config(function ($routeProvider) {
     $routeProvider
@@ -50,18 +58,42 @@ angular.module('blegex').config(function ($routeProvider) {
 angular.module('blegex').controller('HomeController', function ($scope, $location) {
 });
 
-angular.module('blegex').controller('DraftController', function ($scope, $location, $routeParams) {
-    // $routeParams.id;
+angular.module('blegex').controller('InboxController', function ($scope) {
 });
 
-angular.module('blegex').controller('InboxController', function ($scope, $location, $routeParams) {
-    // $routeParams.id;
+angular.module('blegex').controller('DraftController', function ($scope, $routeParams) {
 });
 
-angular.module('blegex').controller('DatabaseController', function ($scope, $location, $routeParams) {
-    // $routeParams.id;
+angular.module('blegex').controller('DatabaseController', function ($scope, $routeParams) {
 });
 
-angular.module('blegex').controller('ConfigurationController', function ($scope, $location, $routeParams) {
-    // $routeParams.id;
+angular.module('blegex').controller('ConfigurationController', function ($scope, $routeParams) {
+});
+
+angular.module('blegex').controller('LoginController', function ($scope, $location, Authentication) {
+
+    $scope.login = function (form) {
+        Authentication.login(form.email, form.password)
+            .then(function () {
+                $location.path('/inbox');
+            });
+    };
+
+});
+
+angular.module('blegex').controller('RegisterController', function ($scope, $location, Authentication) {
+
+    $scope.register = function (form) {
+        Authentication.login(form.email, form.password)
+            .then(function () {
+                $location.path('/inbox');
+            });
+    };
+
+});
+
+angular.module('blegex').controller('LogoutController', function ($scope, Authentication) {
+
+    Authentication.logout();
+
 });
